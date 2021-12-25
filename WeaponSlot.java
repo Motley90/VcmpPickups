2d5a459ade78329d555df2d497f9868c81a57069
Motley90 <motley679@gmail.com>
2021-12-25 01:00:52 -0500

    Update WeaponSlot.java

--- WeaponSlot.java
+++ WeaponSlot.java
@@ -1,15 +1,13 @@
 package com.github.newk5.vcmp.maven.weapons;
-import com.github.newk5.vcmp.maven.controllers.MainController;
+
+import java.util.*;
 import com.maxorator.vcmp.java.plugin.integration.placeable.Pickup;
+import com.github.newk5.vcmp.maven.controllers.MainController;
 
-/*
-  Finish the config
-  Look into Java libraries after to generate respawn time back to world 0
-*/
 
-import java.util.ArrayList;
+
 public class WeaponSlot {
-    public static ArrayList<Data> list = new ArrayList<Data>();
+    public static HashMap<Integer, Data> list = new HashMap<>();
     
     //Nested Class
     class Data {
@@ -27,31 +25,23 @@ public class WeaponSlot {
         this.z = z;
       }
     }
+    
+    // Pickup creation
     public static void main(int iModel, int iPrice, int iWorld, int iRespawnTime, boolean iPickupPicked, float fX, float fY, float fZ) {
         WeaponSlot custom = new WeaponSlot();
         custom.pickupMechanics(iModel, iPrice, iWorld, iRespawnTime, iPickupPicked, fX, fY, fZ);
     }
     public void pickupMechanics(int model, int price, int world, int respawnTime, boolean pickupPicked, float x, float y, float z) {
-        list.add(new Data(model, price, world, respawnTime, pickupPicked, x, y, z));
-        Pickup p = MainController.server.createPickup(model, 0, world, x, y, z, 255, true);
-        int id = p.getId();
-        System.out.println(id);
+      Pickup p = MainController.server.createPickup(model, 0, world, x, y, z, 255, true);
+      int iD = p.getId();
+      list.put(iD, new Data(model, price, world, respawnTime, pickupPicked, x, y, z));
+      System.out.println(iD);
     }
-
-    // Testing the slot
-    public static void getIndex(int slot){
-      try {
-        System.out.println(list.get(slot).model + " " + list.get(slot).price + " " + list.get(slot).world + " " + list.get(slot).respawnTime+ " " + list.get(slot).pickupPicked+ " " + list.get(slot).x+ " " + list.get(slot).y+ " " + list.get(slot).z);
-      } 
-      catch (Exception e) {
-        System.out.println("The index '" + slot + "' does not exist");
-      }
-    }
-
+    // Pickup creation
 }
 
 /*         WeaponSlot.main( 366, 2,  0,  0, false, 0.0f, 0.0f, 0.0f);
-           WeaponSlot.getIndex(0);
-           WeaponSlot.getIndex(2);
-
 */
+
+
+
